1364d52611ada2791e05c1d1c05324e81f3f3db2
Giyosiddin <m_master96@mail.ru>
2020-12-29 11:20:09 +0500

    translation validation

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -148,8 +148,8 @@
 $(document).ready(function(){
 
   $.extend($.validator.messages, {
-      required: "<?php __("This field is required") ?>",
-      email: "Пожалуйста, введите действительный адрес электронной почты."
+      required: "<?php echo __("This field is required"); ?>",
+      email: "<?php echo __('Please enter a valid email address.'); ?>"
 });
 
 $("form.login_form").validate({
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -54,8 +54,8 @@
   $(document).ready(function(){
 
   $.extend($.validator.messages, {
-      required: "<?php __("This field is required") ?>",   
-      email: "Пожалуйста, введите действительный адрес электронной почты."
+      required: "<?php echo __("This field is required") ?>",   
+      email: "<?php echo __('Please enter a valid email address.'); ?>"
 });
 
 $("form.reset_pass").validate({
