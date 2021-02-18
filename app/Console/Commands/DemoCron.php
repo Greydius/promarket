26dd3630a6d6ee42f8d3c46e118d8e23b854e1f7
Giyosiddin <m_master96@mail.ru>
2021-02-18 17:04:53 +0500

    Update DemoCron.php

--- app/Console/Commands/DemoCron.php
+++ app/Console/Commands/DemoCron.php
@@ -4,11 +4,13 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\FixingOrderProduct;
+use App\UnderOrderProduct;
 use App\FixingOrder;
 use Illuminate\Support\Facades\Mail;
 use App\Product;
 
 use App\Mail\UnderProductToClient;
+use App\Mail\UnderOrderMail;
 
 class DemoCron extends Command
 {
@@ -43,33 +45,50 @@ class DemoCron extends Command
      */
     public function handle()
     {
-        $fixingOrders = FixingOrderProduct::all();
-        $orders_products = $fixingOrders->pluck('product_id')->toArray();
-        // $products_id = $fixingOrders->toArray();
-        $products = Product::whereIn('id', [$orders_products])->get()->toArray();
-        foreach($products as $product){
-            if($product['quantity'] !='0') {
-                $fixing_order_products = FixingOrderProduct::where('product_id', $product['id']);
-                // dd($fixing_order_products->get());
-                foreach($fixing_order_products->get() as $check_send){
-                    if($check_send->message_sending == '0'){
 
-                        $order_ids = $fixing_order_products->pluck('fixing_order_id')->toArray();
-                        $fixing_orders = FixingOrder::where('id', '=', $order_ids)->where('id','=',$check_send->fixing_order_id)->pluck('email')->toArray();
-                            \Log::info($check_send);
-                        \DB::table('fixing_order_product')->where('id', $check_send->id)->update(['message_sending' => '1']);
-                        foreach($fixing_orders as $order){
+        $underOrderProducts = UnderOrderProduct::where('is_sent', 0)->get();
 
-                             $name = 'Promarket.lv';
-                             $send = Mail::to($order)->send(new UnderProductToClient($name));
-                            \Log::info($fixing_order_products->get('message_sending'));
-                        }
-                    }
-                }
-            }else{
-                 \Log::info('Product - '.$product['id']. ' quantity='.$product['quantity']);
-                }
+        foreach($underOrderProducts as $orderProduct) {
+            $product = Product::find($orderProduct->product_id);
+            if ($product->quantity > 0) {
+                $order = clone $orderProduct;
+                $order->product = $product;
+                Mail::to($orderProduct->email)->send(new UnderOrderMail($order));
+                $orderProduct->is_sent = 1;
+                $orderProduct->save();
+            }
+
         }
+         \Log::info($underOrderProducts);
+
+
+        // $fixingOrders = UnderOrderProduct::all();
+        // $orders_products = $fixingOrders->pluck('product_id')->toArray();
+        // // $products_id = $fixingOrders->toArray();
+        // $products = Product::whereIn('id', [$orders_products])->get()->toArray();
+        // foreach($products as $product){
+        //     if($product['quantity'] !='0') {
+        //         // $fixing_order_products = FixingOrderProduct::where('product_id', $product['id']);
+        //         // // dd($fixing_order_products->get());
+        //         // foreach($fixing_order_products->get() as $check_send){
+        //         //     if($check_send->message_sending == '0'){
+
+        //         //         $order_ids = $fixing_order_products->pluck('fixing_order_id')->toArray();
+        //         //         $fixing_orders = FixingOrder::where('id', '=', $order_ids)->where('id','=',$check_send->fixing_order_id)->pluck('email')->toArray();
+        //         //             \Log::info($check_send);
+        //         //         \DB::table('fixing_order_product')->where('id', $check_send->id)->update(['message_sending' => '1']);
+        //         //         foreach($fixing_orders as $order){
+
+        //         //              $name = 'Promarket.lv';
+        //         //              $send = Mail::to($order)->send(new UnderProductToClient($name));
+        //         //             \Log::info($fixing_order_products->get('message_sending'));
+        //         //         }
+        //         //     }
+        //         // }
+        //     }else{
+        //          \Log::info('Product - '.$product['id']. ' quantity='.$product['quantity']);
+        //         }
+        // }
 
      
         /*
