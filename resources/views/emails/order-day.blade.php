7c03e5992bd5d520c23106bd37791261ad297016
Giyosiddin <m_master96@mail.ru>
2021-03-24 16:43:08 +0500

    Update order-day.blade.php

--- resources/views/emails/order-day.blade.php
+++ resources/views/emails/order-day.blade.php
@@ -11,6 +11,6 @@
 <?php }else{
         if($tomorrow == $remont_date) {
     ?>
-    <h2>God. klient!<br> Atgadinam, ka rit, <span>{{$details->date}}</span> {{__("at")}}. <b>{{$details->time}}</b> {{__("Your device is logged for service")}} {{$details->branch_name}}. Promarket.lv</h2>
+    <h2>{{__("Dear Customer")}}!<br> {{__("Let's remind you that it goes")}} , <span>{{$details->date}}</span> {{__("at")}}. <b>{{$details->time}}</b> {{__("Your device is logged for service")}} {{$details->branch_name}}. Promarket.lv</h2>
     <? } } ?>
 </div>
