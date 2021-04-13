875a6785b36c1e33b47cab97ca6081ff67d201b3
Giyosiddin <m_master96@mail.ru>
2021-04-13 17:25:14 +0500

    sadasd

--- resources/views/system/master.blade.php
+++ resources/views/system/master.blade.php
@@ -17,6 +17,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="canonical" href="{{Request::url()}}" />
     <meta name="theme-color" content="#fafafa">
 </head>
 
